df17aad80d356ddc255084bdd79bb6124b1bbcd5
Lidia Sánchez <lidia.sm.96@gmail.com>
2022-12-18 20:34:40 +0100

    Añadidas funciones de procesamiento de textos y conversión a bolsa de palabras #6

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,5 +1,7 @@
 import re
 import nltk
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 # Descarga una lista de stopwords
 nltk.download('stopwords')
@@ -7,7 +9,7 @@ nltk.download('stopwords')
 # Lista de stopwords en inglés y español
 STOPWORDS = nltk.corpus.stopwords.words("spanish") + nltk.corpus.stopwords.words("english")
 
-def delete_urls(dataset, column):
+def delete_urls(dataset: pd.DataFrame, column: str):
     """
     Removes URLs and web links from a set of text.
 
@@ -24,7 +26,7 @@ def delete_urls(dataset, column):
     """
     return dataset[column].apply(lambda x: re.split("https:\/\/.*", str(x))[0])
 
-def delete_mentioned_users(dataset, column):
+def delete_mentioned_users(dataset: pd.DataFrame, column: str):
     """
     Removes mentioned users from a set of text.
 
@@ -42,7 +44,7 @@ def delete_mentioned_users(dataset, column):
     return dataset[column].str \
         .replace("@([a-zA-Z0-9_]{1,50})", "", regex=True) 
 
-def delete_hashtags(dataset, column):
+def delete_hashtags(dataset: pd.DataFrame, column: str):
     """
     Removes hashtags and their content from a set of text.
 
@@ -60,7 +62,7 @@ def delete_hashtags(dataset, column):
     return dataset[column].str \
         .replace("#([a-zA-Z0-9_]{1,50})", "", regex=True)
 
-def delete_non_alphabetic_chars(dataset, column):
+def delete_non_alphabetic_chars(dataset: pd.DataFrame, column: str):
     """
     Removes non-alphabetic characters (special characters,
     digits, ...) from a set of text.
@@ -79,7 +81,7 @@ def delete_non_alphabetic_chars(dataset, column):
     return dataset[column].str \
         .replace("[^a-zA-Z ]", "")
 
-def delete_stopwords(dataset, column):
+def delete_stopwords(dataset: pd.DataFrame, column: str):
     """
     Removes stopwords from a set of english and
     spanish texts using the NLTK stopword list.
@@ -98,7 +100,7 @@ def delete_stopwords(dataset, column):
     return dataset[column].apply(lambda x: " ".join(
         [word for word in x.split() if word not in STOPWORDS and len(word) > 1]))
 
-def delete_words_one_char(dataset, column):
+def delete_words_one_char(dataset: pd.DataFrame, column: str):
     """
     Removes words composed of one letter within
     a set of texts.
@@ -117,7 +119,7 @@ def delete_words_one_char(dataset, column):
     return dataset[column].apply(lambda x: " ".join(
         [word for word in x.split() if len(word) > 1]))
 
-def to_lowercase(dataset, column):
+def to_lowercase(dataset: pd.DataFrame, column: str):
     """
     Converts all characters to lowercase within
     a set of text.
@@ -134,3 +136,63 @@ def to_lowercase(dataset, column):
     A Series column
     """
     return dataset[column].str.lower()
+
+def text_processing_pipeline(dataset: pd.DataFrame, column: str):
+    """
+    Applies the next text processing techniques to a text
+    column in a dataset.
+
+    Parameters
+    ----------
+    dataset : Pandas dataframe
+        The data which contains a set of texts.
+    column : str
+        The column name in which the set of texts is stored.
+
+    Returns
+    -------
+    A Pandas dataframe with a new column to store the cleaned text.
+    """
+    # Column name to store the cleaned text
+    cleaned_col = "cleaned_text"
+
+    # Delete URLs
+    dataset[cleaned_col] = delete_urls(dataset, column)
+
+    # Delete mentioned users in tweets
+    dataset[cleaned_col] = delete_mentioned_users(dataset, cleaned_col)
+
+    # Delete hashtags in tweets
+    dataset[cleaned_col] = delete_hashtags(dataset, cleaned_col)
+        
+    # Delete non-alphabetic characters
+    dataset[cleaned_col] = delete_non_alphabetic_chars(dataset, cleaned_col)
+
+    # Delete stopwords for spanish and english texts
+    dataset[cleaned_col] = delete_stopwords(dataset, cleaned_col)
+
+    # Delete one-char words
+    dataset[cleaned_col] = delete_words_one_char(dataset, cleaned_col)
+    
+    # Convert each character to lowercase
+    dataset[cleaned_col] = to_lowercase(dataset, cleaned_col)
+
+    return dataset
+
+def to_bag_of_words(docs: list):
+    """
+    Creates a bag of words converting a list of text documents
+    into numeric vectors by computing the word frequencies
+    per text document.
+
+    Parameters
+    ----------
+    doc: list
+        A list of text documents.
+
+    Returns
+    -------
+    A numpy 2-array.
+    """
+    vectorizer = CountVectorizer()
+    return vectorizer.fit_transform(docs).toarray()

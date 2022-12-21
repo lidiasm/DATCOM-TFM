78d0495b20db559ec89d00f08b98d17933c6fa38
Lidia Sánchez <lidia.sm.96@gmail.com>
2022-12-21 20:50:12 +0100

    Nuevos métodos de lematización y stemming #7

--- scripts/processing.py
+++ scripts/processing.py
@@ -1,8 +1,10 @@
 import re
 import nltk
+import spacy
 import warnings
 import pandas as pd
 from sklearn import preprocessing
+from nltk.stem.snowball import SnowballStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Ignore regex warnins
@@ -142,7 +144,97 @@ def to_lowercase(dataset: pd.DataFrame, column: str):
     """
     return dataset[column].str.lower()
 
-def text_processing_pipeline(dataset: pd.DataFrame, column: str):
+def to_lemmatized_texts(dataset: pd.DataFrame, text_col: str, language_col: str):
+    """
+    Iterates over a set of documents in order to replace 
+    each verb with its infinitive form and each word with 
+    its single form. The lemmatizer to apply depends on the 
+    text language, considering two languages: english and spanish.
+
+    Parameters
+    ----------
+    dataset : Pandas dataframe
+        The data which contains a set of texts to lemmatize.
+    text_col : str
+        The column name in which the set of texts is stored.
+    language_col : str
+        The column name which stores the language of each text.
+
+    Returns
+    -------
+    A list of strings with the lemmatized texts.
+    """
+    # List to save the lemmatized texts
+    lemm_texts = []
+
+    # Create two lemmatizer for english and spanish texts
+    en_lemmatizer = spacy.load("en_core_web_lg")
+    es_lemmatizer = spacy.load("es_core_news_lg")
+
+    # Iterate over the set of texts
+    for record in dataset.to_records("dict"):
+
+        # Select the lemmatizer depending on the text language
+        lemmatizer = en_lemmatizer if record[language_col] == "en" else es_lemmatizer
+
+        # String to save the current text when it's lemmatized
+        current_lemm_text = ""
+
+        # Iterate over the words to be lemmatized
+        for word in lemmatizer(record[text_col]):
+            current_lemm_text += word.lemma_ + " "
+        
+        # Add the lemmatized text to the dataset
+        lemm_texts.append(current_lemm_text)
+    
+    return lemm_texts
+
+def to_stemmed_texts(dataset: pd.DataFrame, text_col: str, language_col: str):
+    """
+    Iterates over a set of documents in order to replace each
+    word with its root form. The stemmer to apply depends on the 
+    text language, considering two languages: english and spanish.
+
+    Parameters
+    ----------
+    dataset : Pandas dataframe
+        The data which contains a set of texts.
+    text_col : str
+        The column name in which the set of texts is stored.
+    language_col : str
+        The column name which stores the language of each text.
+
+    Returns
+    -------
+    A list of strings with the stemmed texts.
+    """
+    # List to save the stemmed texts
+    stemm_texts = []
+
+    # Create two stemmer for english and spanish texts
+    en_stemmer = SnowballStemmer(language="english")
+    es_stemmer = SnowballStemmer(language="spanish")
+
+    # Iterate over the set of texts
+    for record in dataset.to_records("dict"):
+
+        # Select the stemmer depending on the text language
+        stemmer = en_stemmer if record[language_col] == "en" else es_stemmer
+
+        # String to save the current text when it's stemmed
+        current_stemm_text = ""
+
+        # Iterate over the words to be stemmed
+        for word in record[text_col].split(" "):
+            current_stemm_text += stemmer.stem(word) + " "
+        
+        # Add the stemmed text to the dataset
+        stemm_texts.append(current_stemm_text)
+    
+    return stemm_texts
+
+def text_processing_pipeline(dataset: pd.DataFrame, text_col: str, 
+    lemm: bool = False, stemm: bool = False, language_col: str = ""):
     """
     Applies the next text processing techniques to a text
     column in a dataset. 
@@ -159,8 +251,10 @@ def text_processing_pipeline(dataset: pd.DataFrame, column: str):
     ----------
     dataset : Pandas dataframe
         The data which contains a set of texts.
-    column : str
+    text_col : str
         The column name in which the set of texts is stored.
+    language_col : str
+        The column name in which the text languages are stored.
 
     Returns
     -------
@@ -170,7 +264,7 @@ def text_processing_pipeline(dataset: pd.DataFrame, column: str):
     cleaned_col = "cleaned_text"
 
     # Delete URLs
-    dataset[cleaned_col] = delete_urls(dataset, column)
+    dataset[cleaned_col] = delete_urls(dataset, text_col)
 
     # Delete mentioned users in tweets
     dataset[cleaned_col] = delete_mentioned_users(dataset, cleaned_col)
@@ -189,6 +283,14 @@ def text_processing_pipeline(dataset: pd.DataFrame, column: str):
     
     # Convert each character to lowercase
     dataset[cleaned_col] = to_lowercase(dataset, cleaned_col)
+
+    # Lemmatize the texts
+    if (lemm and type(language_col) == str and language_col != ""):
+        dataset[cleaned_col] = to_lemmatized_texts(dataset, cleaned_col, language_col)
+
+    # Text stemming
+    if (stemm and type(language_col) == str and language_col != ""):
+        dataset[cleaned_col] = to_stemmed_texts(dataset, cleaned_col, language_col)
 
     return dataset
 

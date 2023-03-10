6ea717b2a73f8d6e628dfc3d73e9ea3fb6b0b9d7
Lidia Sánchez <lidia.sm.96@gmail.com>
2023-03-10 13:48:58 +0100

    Mejorado el código de las funciones de codificación de textos #4

--- scripts/encoding.py
+++ scripts/encoding.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd 
 import gensim.downloader as api
-from gensim.models import Word2Vec, FastText
+from gensim.models import Word2Vec
 from gensim.models.doc2vec import TaggedDocument, Doc2Vec
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
@@ -25,7 +25,7 @@ def to_bag_of_words(train_docs: list, test_docs: list):
     '''
     bg_vectorizer = CountVectorizer()
     
-    return bg_vectorizer.fit_transform(raw_documents=train_docs), \
+    return bg_vectorizer.fit_transform(raw_documents=train_docs).toarray(), \
         bg_vectorizer.transform(raw_documents=test_docs).toarray()
 
 
@@ -302,74 +302,26 @@ def doc2vec_pipeline(
     return train_d2v_df, test_d2v_df
 
 
-def to_fast_text(
-    docs: list, vector_size: int = 100, window: int = 5, 
-    min_count: int = 5, epochs: int = 5, algorithm: int = 0):
-    '''
-    Creates a word-embedding vector for a train and test texts
-    by training a FastText model able to encode the splitted
-    documents into words to numeric vectors. 
-
-    Parameters
-    ----------
-    docs : list
-        The list of text documents to encode.
-    vector_size : int, optional (default 100)
-        Size of the word embedding vectors.
-    window : int, optional (default 5)
-        Max distance between the current and predicted word 
-        within a sentece.
-    min_count : int, optional (default 5)
-        Ignores words with a frequency lower than this value.
-    epochs : int, optional (default 5)
-        Number of iterations over the set of texts.
-    algorithm : int, optional (default 0)
-        train algorithm: 0 for CBOW, 1 for Skip-Gram.
-
-    Returns
-    -------
-    A list of Numpy ndarray with the word embeddings per document.
-    '''
-    # Split each document into tokens (words) deleting the last whitespace
-    tokens = [doc.split(' ') for doc in docs]
-
-    ft_model = FastText(
-        sentences=tokens,
-        vector_size=vector_size,
-        window=window,
-        min_count=min_count,
-        sg=algorithm,
-        epochs=epochs)
-
-    # Build a set of terms to then train the model
-    ft_vocab = set(ft_model.wv.index_to_key)
-
-    # Create aggregated sentence vectors based on the tokens and FastText vocabulary
-    agg_sentences = np.array([np.array(
-        [
-            ft_model.wv[word] for word in doc if word in ft_vocab
-        ])
-        for doc in tokens
-    ])
-
-    # Normalize sentence vectors using the averaging of the word vectors
-    # for each sentence in order to then be used in ML models
-    return [
-        sent.mean(axis=0) if sent.size else np.zeros(vector_size, dtype=float)
-        for sent in agg_sentences
-    ]
-
-
-def fasttext_pipeline(
+def to_pretrained_embeddings(
         train_df: pd.DataFrame, train_text_col: str,
         test_df: pd.DataFrame, test_text_col: str,
-        vector_size: int = 100, window: int = 5, 
-        min_count: int = 5, epochs: int = 5, alg: int = 0):
+        model: str, vector_size: int = 100):
     '''
-    Creates a train and a test datasets based on the FastText
-    technique to encode a set of texts as word embeddings.
-    These datasets are aimed to be used directly in the building of
-    Machine Learning models.
+    Downloads the provided pre-trained model to then load
+    its pre-trained embeddings ready to use. Following models 
+    are available from `gensim` library:
+        - 'fasttext-wiki-news-subwords-300'
+        - 'conceptnet-numberbatch-17-06-300'
+        - 'word2vec-ruscorpora-300'
+        - 'word2vec-google-news-300'
+        - 'glove-wiki-gigaword-50'
+        - 'glove-wiki-gigaword-100'
+        - 'glove-wiki-gigaword-200'
+        - 'glove-wiki-gigaword-300'
+        - 'glove-twitter-25'
+        - 'glove-twitter-50'
+        - 'glove-twitter-100'
+        - 'glove-twitter-200'
 
     Parameters
     ----------
@@ -381,47 +333,64 @@ def fasttext_pipeline(
         A test dataset to encode.
     test_text_col : str
         A column name in which there are the set of test texts to encode.
+    model : str
+        The name of the pre-trained model to download and load its embeddings.
     vector_size : int, optional (default 100)
         Size of the word embedding vectors.
-    window : int, optional (default 5)
-        Max distance between the current and predicted word 
-        within a sentece.
-    min_count : int, optional (default 5)
-        Ignores words with a frequency lower than this value.
-    epochs : int, optional (default 5)
-        Number of iterations over the set of texts.
-    alg : int, optional (default 0)
-        train algorithm: 0 for CBOW, 1 for Skip-Gram.
 
     Returns
     -------
-    A tuple with the train and test FastText word-embeddings.
+    A tuple with two Pandas dataframes that contain the encoded
+    train and text documents based on the pretrained embeddings.
     '''
-    train_ft_embeddings = to_fast_text(
-        docs=list(train_df[train_text_col].values),
-        vector_size=vector_size,
-        window=window,
-        min_count=min_count,
-        epochs=epochs,
-        algorithm=alg
-    )
-    test_ft_embeddings = to_fast_text(
-        docs=list(test_df[test_text_col].values),
-        vector_size=vector_size,
-        window=window,
-        min_count=min_count,
-        epochs=epochs,
-        algorithm=alg
-    )
+    # Download the pre-trained model if it's not been downloaded before
+    pretrained_model = api.load(name=model)
 
-    train_ft_df = pd.DataFrame(data=train_ft_embeddings)
-    train_ft_df.columns = [
-        f'Feature {index+1}' for index in range(0, train_ft_df.shape[1])
+    # Build a set of terms to then train the model
+    pretrained_vocab = set(pretrained_model.index_to_key)
+
+    # Split the train and test documents into tokens
+    train_tokens = [
+        doc.split(' ') for doc in list(train_df[train_text_col].values)
+    ]
+    test_tokens = [
+        doc.split(' ') for doc in list(test_df[test_text_col].values)
     ]
 
-    test_ft_df = pd.DataFrame(data=test_ft_embeddings)
-    test_ft_df.columns = [
-        f'Feature {index+1}' for index in range(0, test_ft_df.shape[1])
+    # Create aggregated sentence vectors based on the tokens and the pre-trained vocabulary
+    train_agg_sentences = np.array([np.array(
+        [
+            pretrained_model[word] for word in doc if word in pretrained_vocab
+        ])
+        for doc in train_tokens
+    ])
+    test_agg_sentences = np.array([np.array(
+        [
+            pretrained_model[word] for word in doc if word in pretrained_vocab
+        ])
+        for doc in test_tokens
+    ])
+
+    # Normalize sentence vectors using the averaging of the word vectors
+    # for each sentence in order to then be used in ML models
+    train_avg_sentences = [
+        sent.mean(axis=0) if sent.size else np.zeros(vector_size, dtype=float) 
+        for sent in train_agg_sentences
+    ]
+    test_avg_sentences = [
+        sent.mean(axis=0) if sent.size else np.zeros(vector_size, dtype=float) 
+        for sent in test_agg_sentences
     ]
 
-    return train_ft_df, test_ft_df
+    # Create a train and a test datasets with column names
+    train_embeddings_df = pd.DataFrame(data=train_avg_sentences)
+    train_embeddings_df.columns = [
+        f'Feature {index+1}' for index in range(0, train_embeddings_df.shape[1])
+    ]
+
+    test_embeddings_df = pd.DataFrame(data=test_avg_sentences)
+    test_embeddings_df.columns = [
+        f'Feature {index+1}' for index in range(0, test_embeddings_df.shape[1])
+    ]
+
+    return train_embeddings_df, test_embeddings_df
